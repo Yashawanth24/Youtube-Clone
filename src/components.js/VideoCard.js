f1ce053b984442ae0d1759482f26bd03016340e2
Yashawanth <yashawanthbg@gmail.com>
2024-06-18 10:00:27 +0530

    newfills

--- src/components.js/VideoCard.js
+++ src/components.js/VideoCard.js
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { setVideoDetails } from './utlis/appSclice';
 
-const VideoCard = ({info}) => { 
-  console.log(info)
-    const{snippet,statistics}=info;
-    const{thumbnails,channelTitle,title,publishedAt}=snippet;
- const dispatch=useDispatch();
- const handleClick=()=>{
-  dispatch(setVideoDetails({title,channelTitle}))
- };
+const VideoCard = ({ info }) => {
+  const { snippet, statistics } = info;
+  const { thumbnails, channelTitle, title } = snippet;
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(setVideoDetails({ title, channelTitle }));
+  };
+
   return (
-    <div className='p-2 m-2 w-80 shadow-md ' onClick={handleClick}>
+    <div className='p-4 mx-2 my-2 max-w-xs h-96 bg-white rounded-lg shadow-md cursor-pointer' onClick={handleClick}>
+      <img className='rounded-t-lg' src={thumbnails.medium.url} alt={title} />
+      <div className='p-4'>
+        <h3 className='text-lg font-medium leading-tight py-2'>{title}</h3>
+        <p className='text-gray-700 py-1'>{channelTitle}</p>
+        <p className='text-gray-600 py-'>{statistics.viewCount} views</p>
+      </div>
+    </div>
+  );
+};
 
-         <img className='rounded-md' src={thumbnails.medium.url}/>
-         <ul>
-       <li className='font-bold py-2'>{title}</li>
-       <li className='text-lg'>{channelTitle}</li>
-      <li>{statistics.viewCount} views</li>
-       </ul>
-     </div>
-  )
-
-}
-
-export default VideoCard
+export default VideoCard;
--- src/components.js/WatchPage.js
+++ src/components.js/WatchPage.js
@@ -1,32 +1,47 @@
-import React, { useEffect } from 'react'
-import { closeMenu } from './utlis/appSclice'
-import { useDispatch, useSelector } from 'react-redux'
-import {  useSearchParams } from 'react-router-dom'
-import CommentContainer from './utlis/CommentContainer'
+import React, { useEffect } from 'react';
+import { closeMenu } from './utlis/appSclice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
+import CommentContainer from './utlis/CommentContainer';
 
 const WatchPage = () => {
   
-   const [searchparms]=useSearchParams();
-    const dispatch=useDispatch();
-    const videoDetails=useSelector((state)=>state.app.videoDetails)
-    console.log(videoDetails.title)
-    useEffect(()=>{
-   dispatch(closeMenu())
-    },[dispatch])
-  return (
-    <div className='flex flex-col'>
-    <div className='px-5 '><iframe width="1000" height="500" src={"https://www.youtube.com/embed/"+searchparms.get("v")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe></div>
-    {videoDetails && (
-        <div className='px-5'>
-      
-          <h1 className='font-bold text-xl'>{videoDetails.title}</h1>
-          <h2 className='text-lg font-bold font-serif py-4 mx-8'>{videoDetails.channelTitle}</h2>
-        </div>
-      )}
+   const [searchParams] = useSearchParams();
+   const dispatch = useDispatch();
+   const videoDetails = useSelector((state) => state.app.videoDetails);
+   
+   useEffect(() => {
+     dispatch(closeMenu());
+   }, [dispatch]);
 
-    <CommentContainer videoId={searchparms.get("v")}/>
-    </div>
-  )
+   return (
+     <div className="flex flex-col items-center bg-gray-100 min-h-screen p-5">
+       <div className="w-full max-w-4xl">
+         <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+           <iframe 
+             width="100%" 
+             height="500" 
+             src={"https://www.youtube.com/embed/" + searchParams.get("v")} 
+             title="YouTube video player" 
+             frameBorder="0" 
+             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+             referrerPolicy="strict-origin-when-cross-origin" 
+             allowFullScreen
+             className="w-full"
+           ></iframe>
+         </div>
+         {videoDetails && (
+           <div className="bg-white shadow-lg rounded-lg p-5 mt-5">
+             <h1 className="font-bold text-2xl mb-2">{videoDetails.title}</h1>
+             <h2 className="text-lg font-medium text-gray-700">{videoDetails.channelTitle}</h2>
+           </div>
+         )}
+         <div className="bg-white shadow-lg rounded-lg p-5 mt-5 w-full">
+           <CommentContainer videoId={searchParams.get("v")} />
+         </div>
+       </div>
+     </div>
+   );
 }
 
-export default WatchPage
+export default WatchPage;
